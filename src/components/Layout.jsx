79bfa181211e4f2ca67de25e7289db1f66654276
abhaychhani <abhaychhani@gmail.com>
2025-08-29 13:17:28 +0530

    precent functionality fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,8 @@ function Layout() {
 
   const hadleCalc = ()=>{
     try {
-      setResult(eval(calculation));
+      let calcString = calculation.replace('%','*1/100').replace('x','*');
+      setResult(eval(calcString));
     } catch (error) {
       alert("please Enter valid calulation")
     }
@@ -23,9 +24,9 @@ function Layout() {
           setCalculation("")
           setResult(null)
           }}>Clean</button>
-        <button className="operator" onClick={()=>setCalculation(prev=>prev+"/")}>/</button>
-        <button className="operator" onClick={()=>setCalculation(prev=>prev+"*")}>X</button>
-        <button className="operator" onClick={()=>setCalculation(prev=>prev+"-")}>-</button>
+        <button className="operator" onClick={()=>setCalculation(prev=>prev+"/")} >/</button>
+        <button className="operator" onClick={()=>setCalculation(prev=>prev+"x")} >x</button>
+        <button className="operator" onClick={()=>setCalculation(prev=>prev+"-")} >-</button>
 
         <button onClick={()=>setCalculation(prev=>prev+"7")}>7</button>
         <button onClick={()=>setCalculation(prev=>prev+"8")}>8</button>
@@ -40,7 +41,9 @@ function Layout() {
         <button onClick={()=>setCalculation(prev=>prev+"1")}>1</button>
         <button onClick={()=>setCalculation(prev=>prev+"2")}>2</button>
         <button onClick={()=>setCalculation(prev=>prev+"3")}>3</button>
-        <button onClick={()=>setCalculation(prev=>prev.slice(0,[prev.length-1]))} className="cut">x</button>
+        <button onClick={()=>setCalculation(prev=>prev.slice(0,[prev.length-1]))} className="cut">
+          <span>x</span>
+          </button>
 
         <button onClick={()=>setCalculation(prev=>prev+".")}>.</button>
         <button onClick={()=>setCalculation(prev=>prev+"0")}>0</button>
